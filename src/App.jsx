9ac8a326a25c8b6aa656a485305fde338713ac71
Karen Mtz521 <karen_mf123@hotmail.com>
2025-08-05 23:02:31 -0600

    elimina useData

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import * as yup from 'yup'
 import axios from 'axios'
 import { useState } from 'react'
-import { useData } from './hooks/useData'
 
 const schema = yup.object({
   userInput: yup
@@ -20,8 +19,6 @@ export const App = () => {
   // Guarda la respuesta de llama2
   const [response, setResponse] = useState('')
   const [loading, setLoading] = useState(false)
-  const { multiplicar } = useData()
-  console.log(multiplicar(5))
 
   const handlePregunta = async (data) => {
     console.log(data)
--- src/hooks/useData.js
+++
@@ -1,7 +0,0 @@
-export const useData = () => {
-  const multiplicar = (num) => {
-    return num * num
-  }
-
-  return { multiplicar }
-}
