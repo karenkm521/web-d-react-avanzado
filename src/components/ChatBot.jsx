f25bc39d4ad1044aeee0197e52f8ac1dfc4f09ba
Karen Mtz521 <karen_mf123@hotmail.com>
2025-08-20 20:15:33 -0600

    estilos a chatbot

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -2,8 +2,9 @@ import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useContext } from 'react'
-import { ChatContext } from '../context/chatContext'
+import { ChatContext } from '../context/ChatContext'
 import { useOllama } from '../hooks/useOllama'
+import '../ChatBot.css'
 
 const schema = yup.object({
   userInput: yup
@@ -34,31 +35,28 @@ export const ChatBot = () => {
   }
 
   return (
-    <>
+    <div className='chat-container'>
+
+      <div className='chat-messages'>
+        {state.messages.map((msg, index) => (
+          <div
+            key={index}
+            className={`message ${msg.from === 'user' ? 'user' : 'bot'}`}
+          >
+            {msg.text}
+          </div>
+        ))}
+        {state.loading && <p className='loading'>Generando respuesta 🚀</p>}
+      </div>
       <form onSubmit={handleSubmit(handlePregunta)}>
         <input
           type='text'
           {...register('userInput')}
-          className='w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400'
+          placeholder='Escribe tu mensaje...'
         />
-        {errors.userInput && <p>{errors.userInput.message}</p>}
-        <button
-          className='w-full py-2 rounded transition cursor-pointer bg-blue-600 text-white hover:bg-blue-700'
-        >Preguntar
-        </button>
+        <button type='submit'>Preguntar</button>
       </form>
-      {/* <div>
-        <p>{loading ? 'Generando respuesta 🚀' : response}</p>
-      </div> */}
-      <div>
-        {state.messages.map((msg, index) => (
-          <p key={index}>
-            <strong>{msg.from === 'user' ? 'Tú' : 'Bot'}:</strong>
-            {msg.text}
-          </p>
-        ))}
-        {state.loading && <p>Generando respuesta 🚀 </p>}
-      </div>
-    </>
+      {errors.userInput && <p>{errors.userInput.message}</p>}
+    </div>
   )
 }
