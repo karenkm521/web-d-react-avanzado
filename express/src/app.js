3289d609dc522af68fca3b9e32c586812f093080
Karen Mtz521 <karen_mf123@hotmail.com>
2025-08-24 21:48:29 -0600

    Servidor con Express

--- express/src/app.js
+++ express/src/app.js
@@ -1,14 +1,57 @@
+require('dotenv').config()
+// console.log(process.env.PORT)
+// console.log(process.env.NOMBRE)
 // Importamos el módulo de Express
 const express = require('express')
+const { infoPeliculas } = require('./peliculas')
 
 // Creamos una aplicación de Express
 const app = express()
 
 // Definimos el puerto que va a escuchar el servidor
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hola mundo')
+})
+
+app.get('/api/peliculas', (req, res) => {
+  res.send(infoPeliculas)
+})
+
+app.get('/api/peliculas/accion/titulo/:titulo/:year', (req, res) => {
+  /* const titulo = req.params.titulo
+  const year = req.params.year */
+  const { titulo, year } = req.params
+  const resultados = infoPeliculas.accion.filter(pelicula => pelicula.titulo === titulo && pelicula.year === Number(year))
+
+  if (resultados.length === 0) {
+    return res.status(400).send(`No se encontraron resultados para ${titulo} en el año ${year}`)
+  }
+
+  res.send(resultados)
+})
+
+app.get('/api/peliculas/comedia/:pais', (req, res) => {
+  const pais = req.params.pais
+  const resultados = infoPeliculas.comedia.filter(pelicula => pelicula.pais === pais)
+
+  if (req.query.ordenar === 'year') {
+    return res.send(resultados.sort((a, b) => b.year - a.year))
+  }
+
+  res.send(resultados)
+})
+// Recibir informacion
+app.use(express.json())
+app.post('/api/peliculas', (req, res) => {
+  const nuevaPelicula = req.body
+
+  console.log(nuevaPelicula)
+  res.status(201).send({
+    mensaje: 'La película se recibió con éxito',
+    datos: nuevaPelicula
+  })
 })
 
 app.listen(PORT, () => {
---
+++ express/src/peliculas.js
@@ -0,0 +1,44 @@
+const infoPeliculas = {
+  accion: [
+    {
+      id: 1,
+      titulo: 'Mision imposible',
+      year: 2015,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'John Wick',
+      year: 2014,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: 'Mad Max: Fury Road',
+      year: 2015,
+      pais: 'australia'
+    }
+  ],
+  comedia: [
+    {
+      id: 1,
+      titulo: 'Superbad',
+      year: 2007,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'The Hangover',
+      year: 2009,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: 'Jojo Rabbit',
+      year: 2019,
+      pais: 'usa'
+    }
+  ]
+}
+
+module.exports.infoPeliculas = infoPeliculas
